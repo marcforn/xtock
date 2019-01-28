61e17a70d5ed303b18b6d3f56fb215d56ad7721b
mforn <marc.forn@gmail.com>
2019-01-28 20:36:05 +0100

    Add RetrofitAdapter class

---
+++ data/src/main/java/mforn/io/xtock/webservice/configuration/RetrofitAdapter.kt
@@ -0,0 +1,41 @@
+package mforn.io.xtock.webservice.configuration
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import mforn.io.xtock.data.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitAdapter {
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(setOkHttpClient())
+        .build()
+
+    private fun setOkHttpClient(): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+
+        // Logging Okhhtp requests
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
+        httpClient.addInterceptor(logging)
+
+        // Setting Connection Timeout
+        httpClient.connectTimeout(TIME_OUT_MILLIS, TimeUnit.MILLISECONDS)
+        httpClient.writeTimeout(TIME_OUT_MILLIS, TimeUnit.MILLISECONDS)
+        httpClient.readTimeout(TIME_OUT_MILLIS, TimeUnit.MILLISECONDS)
+
+        //Sets Redirects
+        httpClient.followRedirects(true)
+        httpClient.followSslRedirects(true)
+
+        return httpClient.build()
+    }
+
+}
